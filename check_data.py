f9f7283b6b2f229dc5d50da8c2b09fe4e3021ab9
Martin <martin_cho@pegatroncorp.com>
2024-03-01 15:37:33 +0800

    add files

---
+++ check_data.py
@@ -0,0 +1,64 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import matplotlib.pyplot as plt
+
+
+for dirname, _, filenames in os.walk('data/train'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+def draw(x,y):
+    fig = plt.figure()
+    axes = fig.add_subplot(1,1,1)
+    axes.plot(x,y)
+    plt.show()
+
+
+df = pd.read_csv("data/test/test.csv")
+print("test:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("================")
+
+
+df = pd.read_csv("data/train/train.csv")
+print("train:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("================")
+
+
+df = pd.read_csv('data/train/oil.csv')
+print("oil:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("------------------")
+print(df[df.dcoilwtico.isnull()])
+print("================")
+draw(df['date'],df['dcoilwtico'])
+df_fill = df.fillna(method="pad")
+draw(df_fill['date'],df_fill['dcoilwtico'])
+
+
+df = pd.read_csv('data/train/stores.csv')
+print("stores:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("================")
+
+df = pd.read_csv('data/train/transactions.csv')
+print("transactions:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("================")
+
+
+df = pd.read_csv('data/train/holidays_events.csv')
+print("holidays_events:",df.shape)
+print(df.head())
+print(df.isnull().sum())
+print("================")
+
+# ax=df.plot.kde()
