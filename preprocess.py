c94b40de068185f39e2b539069b2096705493123
Martin <martin_cho@pegatroncorp.com>
2024-03-18 11:37:07 +0800

    update read_df

--- preprocess.py
+++ preprocess.py
@@ -9,86 +9,84 @@ def draw(x,y):
     axes.plot(x,y)
     plt.show()
 
+def show_missing_values(datasets):
+    for df_name, data in datasets.items():
+        print('=' * 30)
+        print(f"Check missing values of {df_name} dataset:")
+        print(data.isnull().sum())
 
-def merge_df():
-    df = pd.read_csv('data/train/oil.csv')
-    # print(df.isna().sum())
-    # draw(df['date'],df['dcoilwtico'])
-    df_oil_fill = df.fillna(method="backfill")
-
-    df_train = pd.read_csv("data/train/train.csv")
-    df_test = pd.read_csv("data/test/test.csv")
-    df_holidays_events = pd.read_csv("data/train/holidays_events.csv")
-    df_holidays_events.rename(columns={'type':'holiday_type'},inplace=True)
-    df_stores = pd.read_csv("data/train/stores.csv")
-    df_stores.rename(columns={'type':'store_type'},inplace=True)
-    df_transactions = pd.read_csv("data/train/transactions.csv")
-
-    # Converting the 'date' column in the datasets to datetime format
-    # Train dataset
-    df_train['date'] = pd.to_datetime(df_train['date'])
-
-    # Test dataset
-    df_test['date'] = pd.to_datetime(df_test['date'])
-
-    # Holiday Events dataset
-    df_holidays_events['date'] = pd.to_datetime(df_holidays_events['date'])
-
-    # Oil dataset
-    df_oil_fill['date'] = pd.to_datetime(df_oil_fill['date'])
-
-    # Transactions dataset
-    df_transactions['date'] = pd.to_datetime(df_transactions['date'])
-    # Confirm the data type of the 'date' column after transformation
-    print('Date Column Data Type After Transformation:') 
-    print('='*30)
-    print("Train dataset:", df_train['date'].dtype)
-    print("Test dataset:", df_test['date'].dtype)
-    print("Holiday Events dataset:", df_holidays_events['date'].dtype)
-    print("Oil dataset:", df_oil_fill['date'].dtype)
-    print("Transactions dataset:", df_transactions['date'].dtype)
-
-    min_date = df_train['date'].min()
-    max_date = df_train['date'].max()
+def check_dates(dataframe):
+    min_date = dataframe['date'].min()
+    max_date = dataframe['date'].max()
     expected_dates = pd.date_range(start=min_date, end=max_date)
-    missing_dates = expected_dates[~expected_dates.isin(df_train['date'])]
+    missing_dates = expected_dates[~expected_dates.isin(dataframe['date'])]
     if len(missing_dates) == 0:
         print("The date data is complete.")
     else:
         print("The date data is incomplete. missing dates:")
         print(missing_dates)
+    # The date data is incomplete. missing dates:
+    # DatetimeIndex(['2013-12-25', '2014-12-25', '2015-12-25', '2016-12-25'], dtype='datetime64[ns]', freq=None)
 
-    # Check for missing values in the datasets
-    datasets = {'train': df_train, 'test': df_test, 'holiday events': df_holidays_events, 'oil': df_oil_fill, 'stores': df_stores, 'transactions': df_transactions, }
-
-    # def show_missing_values(datasets):
-    #     for name, data in datasets.items():
-    #         print(f"Missing values in the {name.capitalize()} dataset:")
-    #         print(data.isnull().sum())
-    #         print('=' * 30)
-    #         print()
-
-    # show_missing_values(datasets)
-    # # Complete the missing dates in the train dataset
-    # # Create an index of the missing dates as a DatetimeIndex object
-    # missing_dates = pd.Index(['2013-12-25', '2014-12-25', '2015-12-25', '2016-12-25'], dtype='datetime64[ns]')
+def insert_dates(dataframe):
+    min_date = dataframe['date'].min()
+    max_date = dataframe['date'].max()
+    expected_dates = pd.date_range(start=min_date, end=max_date)
+    missing_dates = expected_dates[~expected_dates.isin(dataframe['date'])]
+    # Create an index of the missing dates as a DatetimeIndex object
     # Create a DataFrame with the missing dates, using the 'date' column
     missing_data = pd.DataFrame({'date': missing_dates})
     # Concatenate the original train dataset and the missing data DataFrame
     # ignore_index=True ensures a new index is assigned to the resulting DataFrame
-    df_train = pd.concat([df_train, missing_data], ignore_index=True)
+    dataframe = pd.concat([dataframe, missing_data], ignore_index=True)
     # Sort the DataFrame based on the 'date' column in ascending order
-    df_train.sort_values('date', inplace=True)
+    dataframe.sort_values('date', inplace=True)
 
-    min_date = df_train['date'].min()
-    max_date = df_train['date'].max()
-    expected_dates = pd.date_range(start=min_date, end=max_date)
-    missing_dates = expected_dates[~expected_dates.isin(df_train['date'])]
-    if len(missing_dates) == 0:
-        print("The date data is complete.")
+    return dataframe
 
+def read_df():
+    df_oil = pd.read_csv('data/train/oil.csv')
+    df_train = pd.read_csv("data/train/train.csv")
+    df_test = pd.read_csv("data/test/test.csv")
+    df_holidays_events = pd.read_csv("data/train/holidays_events.csv")
+    df_stores = pd.read_csv("data/train/stores.csv")
+    df_transactions = pd.read_csv("data/train/transactions.csv")
 
+    # Check for missing values in the datasets
+    datasets_1 = {'train': df_train, 'test': df_test, 'holiday events': df_holidays_events, 'oil': df_oil, 'stores': df_stores, 'transactions': df_transactions}
+    show_missing_values(datasets_1)
 
+    # fill df_oil missing data
+    # draw(df_oil['date'],df_oil['dcoilwtico'])
+    df_oil = df_oil.fillna(method="backfill")
+    # draw(df_oil['date'],df_oil['dcoilwtico'])
+
+    # Converting the 'date' column in the datasets to datetime format
+    datasets_2 = {'train': df_train, 'test': df_test, 'holiday events': df_holidays_events, 'oil': df_oil, 'stores': df_stores, 'transactions': df_transactions}
+    for df_name, dataframe in datasets_2.items():
+        if 'date' in dataframe.columns:
+            dataframe['date'] = pd.to_datetime(dataframe['date'])
+            print(f"{df_name} date: {dataframe['date'].dtype}")
+
+    # check date data
+    check_dates(df_train)
+    # insert dates
+    df_train = insert_dates(df_train)
+    check_dates(df_train)
+
+    df_holidays_events.rename(columns={'type':'holiday_type'},inplace=True)
+    df_stores.rename(columns={'type':'store_type'},inplace=True)
+
+    merged_df = df_train.merge(df_stores,on='store_nbr',how='inner')
+    merged_df = merged_df.merge(df_transactions,on=['date','store_nbr'],how='inner')
+    merged_df = merged_df.merge(df_holidays_events,on='date',how='inner')
+    merged_df = merged_df.merge(df_oil,on='date',how='inner')
+    print(f"merged train df shape:{merged_df.shape}")
+    print(f"merged test df shape:{df_test.shape}")
+
+    return merged_df, df_test
+
+def merge_df():
     # train data
     df_train_new = pd.merge(df_train,df_oil_fill,how='left',on='date')
     # draw(df_train_new['date'],df_train_new['dcoilwtico'])
@@ -192,9 +190,9 @@ def prework(df_train_new,df_test_new):
 
 
 if __name__ == "__main__":
-    print(f"{'='*10} start {'='*10}")
-    df_train_new,df_test_new = merge_df()
-    df_train_new,df_test_new = prework(df_train_new,df_test_new)
+    print(f"{'-'*10} start {'-'*10}")
+    df_train_new,df_test_new = read_df()
+    # df_train_new,df_test_new = prework(df_train_new,df_test_new)
     # print(f"{'-'*10} df_train_new {'-'*10}")
     # check_na(df_train_new)
     # print(f"{'-'*10} df_test_new {'-'*10}")
